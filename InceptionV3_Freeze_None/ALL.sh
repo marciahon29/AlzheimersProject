8011f99fde8a9c051cb93c9022645b463c62f72d
marciahon29 <marcia.hon.29@gmail.com>
2018-01-27 10:41:38 -0500

    Add files via upload

---
+++ InceptionV3_Freeze_None/ALL.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python InceptionV3_Freeze_00.py > InceptionV3_freeze_00_none_results.txt
+python InceptionV3_Freeze_01.py > InceptionV3_freeze_01_none_results.txt
+python InceptionV3_Freeze_02.py > InceptionV3_freeze_02_none_results.txt
+python InceptionV3_Freeze_03.py > InceptionV3_freeze_03_none_results.txt
+python InceptionV3_Freeze_04.py > InceptionV3_freeze_04_none_results.txt
+python InceptionV3_Freeze_05.py > InceptionV3_freeze_05_none_results.txt
