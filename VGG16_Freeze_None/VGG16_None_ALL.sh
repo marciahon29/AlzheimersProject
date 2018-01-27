831aa55a25b105ca224cf85427493973fbbf2484
marciahon29 <marcia.hon.29@gmail.com>
2018-01-27 10:37:13 -0500

    Add files via upload

---
+++ VGG16_Freeze_None/VGG16_None_ALL.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python VGG16_freeze_02.py > VGG16_freeze_none_02_results.txt
+python VGG16_freeze_03.py > VGG16_freeze_none_03_results.txt
+python VGG16_freeze_04.py > VGG16_freeze_none_04_results.txt
+python VGG16_freeze_05.py > VGG16_freeze_none_05_results.txt
