62f950029ba9ccace021f6d6bafd211cc957b82d
marciahon29 <marcia.hon.29@gmail.com>
2018-02-24 11:14:41 -0500

    Add files via upload

---
+++ 64_Images/ADvsNormal/ADvsNM_64_InceptionV3_None/ADvsNM_64_InceptionV3_None_All.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python ADvsNM_64_InceptionV3_None_data0.py > ADvsNM_64_InceptionV3_None_data0_results.txt
+python ADvsNM_64_InceptionV3_None_data1.py > ADvsNM_64_InceptionV3_None_data1_results.txt
+python ADvsNM_64_InceptionV3_None_data2.py > ADvsNM_64_InceptionV3_None_data2_results.txt
+python ADvsNM_64_InceptionV3_None_data3.py > ADvsNM_64_InceptionV3_None_data3_results.txt
+python ADvsNM_64_InceptionV3_None_data4.py > ADvsNM_64_InceptionV3_None_data4_results.txt
+
