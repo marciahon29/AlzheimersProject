b26c6be40e0d8b8eb380a37b851cd349e09fc8b7
marciahon29 <marcia.hon.29@gmail.com>
2018-02-24 12:04:25 -0500

    Add files via upload

---
+++ 32_Images/MCIvsNormal/MCvsNM_32_InceptionV3_None/MCvsNM_32_InceptionV3_None_All.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python MCvsNM_32_InceptionV3_None_data0.py > MCvsNM_32_InceptionV3_None_data0_results.txt
+python MCvsNM_32_InceptionV3_None_data1.py > MCvsNM_32_InceptionV3_None_data1_results.txt
+python MCvsNM_32_InceptionV3_None_data2.py > MCvsNM_32_InceptionV3_None_data2_results.txt
+python MCvsNM_32_InceptionV3_None_data3.py > MCvsNM_32_InceptionV3_None_data3_results.txt
+python MCvsNM_32_InceptionV3_None_data4.py > MCvsNM_32_InceptionV3_None_data4_results.txt
+
