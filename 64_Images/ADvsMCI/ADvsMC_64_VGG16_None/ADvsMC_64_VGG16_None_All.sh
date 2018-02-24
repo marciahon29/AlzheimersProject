f6d710efdf3fff4bd37683a13a187dedb6f249c5
marciahon29 <marcia.hon.29@gmail.com>
2018-02-24 10:55:04 -0500

    Add files via upload

---
+++ 64_Images/ADvsMCI/ADvsMC_64_VGG16_None/ADvsMC_64_VGG16_None_All.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python ADvsMC_64_VGG16_None_data0.py > ADvsMC_64_VGG16_None_data0_results.txt
+python ADvsMC_64_VGG16_None_data1.py > ADvsMC_64_VGG16_None_data1_results.txt
+python ADvsMC_64_VGG16_None_data2.py > ADvsMC_64_VGG16_None_data2_results.txt
+python ADvsMC_64_VGG16_None_data3.py > ADvsMC_64_VGG16_None_data3_results.txt
+python ADvsMC_64_VGG16_None_data4.py > ADvsMC_64_VGG16_None_data4_results.txt
+
