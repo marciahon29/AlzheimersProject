b4379647b8b7dafe6467dfd4307993c2a8c784c1
marciahon29 <marcia.hon.29@gmail.com>
2018-03-04 11:41:32 -0500

    Add files via upload

---
+++ 16_Images/MCIvsNormal/MCvsNM_16_VGG16_None/MCvsNM_16_VGG16_None_All.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python MCvsNM_16_VGG16_None_data0.py > MCvsNM_16_VGG16_None_data0_results.txt
+python MCvsNM_16_VGG16_None_data1.py > MCvsNM_16_VGG16_None_data1_results.txt
+python MCvsNM_16_VGG16_None_data2.py > MCvsNM_16_VGG16_None_data2_results.txt
+python MCvsNM_16_VGG16_None_data3.py > MCvsNM_16_VGG16_None_data3_results.txt
+python MCvsNM_16_VGG16_None_data4.py > MCvsNM_16_VGG16_None_data4_results.txt
+
