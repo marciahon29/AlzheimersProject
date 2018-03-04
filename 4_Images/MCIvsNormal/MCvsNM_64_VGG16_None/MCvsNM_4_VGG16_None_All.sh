f1bc968420804ce97c066d44c446193c5816de3d
marciahon29 <marcia.hon.29@gmail.com>
2018-03-04 11:03:56 -0500

    Delete MCvsNM_4_VGG16_None_All.sh

--- 4_Images/MCIvsNormal/MCvsNM_64_VGG16_None/MCvsNM_4_VGG16_None_All.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python MCvsNM_4_VGG16_None_data0.py > MCvsNM_4_VGG16_None_data0_results.txt
-python MCvsNM_4_VGG16_None_data1.py > MCvsNM_4_VGG16_None_data1_results.txt
-python MCvsNM_4_VGG16_None_data2.py > MCvsNM_4_VGG16_None_data2_results.txt
-python MCvsNM_4_VGG16_None_data3.py > MCvsNM_4_VGG16_None_data3_results.txt
-python MCvsNM_4_VGG16_None_data4.py > MCvsNM_4_VGG16_None_data4_results.txt
-
