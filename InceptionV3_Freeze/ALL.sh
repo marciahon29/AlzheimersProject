f791d8c9a6e956d6bbd0bf0351449a41329f2954
marciahon29 <marcia.hon.29@gmail.com>
2018-06-01 15:38:48 -0400

    Delete ALL.sh

--- InceptionV3_Freeze/ALL.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python InceptionV3_Freeze_00.py > InceptionV3_freeze_00_results.txt
-python InceptionV3_Freeze_01.py > InceptionV3_freeze_01_results.txt
-python InceptionV3_Freeze_02.py > InceptionV3_freeze_02_results.txt
-python InceptionV3_Freeze_03.py > InceptionV3_freeze_03_results.txt
-python InceptionV3_Freeze_04.py > InceptionV3_freeze_04_results.txt
-python InceptionV3_Freeze_05.py > InceptionV3_freeze_05_results.txt
