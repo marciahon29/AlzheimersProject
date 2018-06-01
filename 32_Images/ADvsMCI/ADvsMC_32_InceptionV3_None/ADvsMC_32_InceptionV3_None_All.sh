91eb8644f13551b5f88ec93facaddfd313886135
marciahon29 <marcia.hon.29@gmail.com>
2018-06-01 15:58:17 -0400

    Delete ADvsMC_32_InceptionV3_None_All.sh

--- 32_Images/ADvsMCI/ADvsMC_32_InceptionV3_None/ADvsMC_32_InceptionV3_None_All.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python ADvsMC_32_InceptionV3_None_data0.py > ADvsMC_32_InceptionV3_None_data0_results.txt
-python ADvsMC_32_InceptionV3_None_data1.py > ADvsMC_32_InceptionV3_None_data1_results.txt
-python ADvsMC_32_InceptionV3_None_data2.py > ADvsMC_32_InceptionV3_None_data2_results.txt
-python ADvsMC_32_InceptionV3_None_data3.py > ADvsMC_32_InceptionV3_None_data3_results.txt
-python ADvsMC_32_InceptionV3_None_data4.py > ADvsMC_32_InceptionV3_None_data4_results.txt
-
